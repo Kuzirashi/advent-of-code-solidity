942e21a700bd2e9439303808e8b24600ca00f9d3
Daniel Kmak <4950658+Kuzirashi@users.noreply.github.com>
2024-01-16 15:27:11 +0100

    Day 2 part 1: optimize 37% of gas usage

--- src/Day02_01.sol
+++ src/Day02_01.sol
@@ -8,10 +8,13 @@ import {BytesLib} from "solidity-bytes-utils/BytesLib.sol";
 import {LibString} from "solady/src/utils/LibString.sol";
 
 contract Day02_01 {
-    function parseLine(string memory line) public view virtual returns (uint256) {
-        string[] memory split = LibString.split(line, ":");
-        string[] memory gameHeaderSplit = LibString.split(split[0], " ");
-        string[] memory gameDataSplit = LibString.split(split[1], ";");
+    uint8 constant MAXIMUM_ALLOWED_RED = 12;
+    uint8 constant MAXIMUM_ALLOWED_GREEN = 13;
+    uint8 constant MAXIMUM_ALLOWED_BLUE = 14;
+
+    function parseLine(string calldata line) public view virtual returns (uint256) {
+        uint256 indexOfColon = LibString.indexOf(line, ":", 0);
+        string[] memory gameDataSplit = LibString.split(string(bytes(line[indexOfColon:])), ";");
 
         uint256 maxRed;
         uint256 maxBlue;
@@ -22,26 +25,40 @@ contract Day02_01 {
 
             for (uint256 j; j < splitByComma.length; j++) {
                 string[] memory cubeTupleSplit = LibString.split(splitByComma[j], " ");
-                uint256 cubeAmount = stringToUint(cubeTupleSplit[1]);
+                uint256 cubeAmount = bytesToUint(bytes(cubeTupleSplit[1]));
                 uint256 cubeColorLength = bytes(cubeTupleSplit[2]).length;
 
                 if (cubeColorLength == 3 && cubeAmount > maxRed) {
+                    if (cubeAmount > MAXIMUM_ALLOWED_RED) {
+                        return 0;
+                    }
+
                     maxRed = cubeAmount;
                 } else if (cubeColorLength == 4 && cubeAmount > maxBlue) {
+                    if (cubeAmount > MAXIMUM_ALLOWED_BLUE) {
+                        return 0;
+                    }
+
                     maxBlue = cubeAmount;
                 } else if (cubeColorLength == 5 && cubeAmount > maxGreen) {
+                    if (cubeAmount > MAXIMUM_ALLOWED_GREEN) {
+                        return 0;
+                    }
+
                     maxGreen = cubeAmount;
                 }
             }
         }
 
-        if (maxBlue <= 14 && maxGreen <= 13 && maxRed <= 12) {
-            return stringToUint(gameHeaderSplit[1]);
+        if (maxBlue <= MAXIMUM_ALLOWED_BLUE && maxGreen <= MAXIMUM_ALLOWED_GREEN && maxRed <= MAXIMUM_ALLOWED_RED) {
+            bytes calldata gameIdBytes = bytes(line[5:indexOfColon]);
+            return bytesToUint(gameIdBytes);
         }
+
+        return 0;
     }
 
-    function stringToUint(string memory s) public pure returns (uint256) {
-        bytes memory b = bytes(s);
+    function bytesToUint(bytes memory b) public pure returns (uint256) {
         uint256 result = 0;
         for (uint256 i = 0; i < b.length; i++) {
             uint256 c = uint256(uint8(b[i]));
