30eb7b709513bf0649df609cf999def90de6c537
Daniel Kmak <4950658+Kuzirashi@users.noreply.github.com>
2024-01-17 19:05:50 +0100

    uin256 instead of uint8 to save gas

--- src/Day01_01.sol
+++ src/Day01_01.sol
@@ -6,13 +6,13 @@ import {LibBytesUint} from "./LibBytesUint.sol";
 contract Day01_01 {
     function parseLine(string memory line) public view virtual returns (uint256) {
         bytes memory lineBytes = bytes(line);
-        uint8 firstDigit;
+        uint256 firstDigit;
         bool firstDigitSet;
-        uint8 lastDigit;
+        uint256 lastDigit;
 
-        for (uint256 index; index < lineBytes.length; index++) {
-            if (LibBytesUint.isDigit(lineBytes[index])) {
-                uint8 digit = uint8(bytes1(lineBytes[index])) - 48;
+        for (uint256 i; i < lineBytes.length; i++) {
+            if (LibBytesUint.isDigit(lineBytes[i])) {
+                uint256 digit = uint256(uint8(bytes1(lineBytes[i]))) - 48;
                 if (!firstDigitSet) {
                     firstDigit = digit;
                     firstDigitSet = true;
